6b76a5878b627595ea8b7191d01abcf314be55b2
zhangchaoran <2267163211@qq.com>
2019-09-27 17:10:56 +0800

    初始化项目

---
+++ project/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    123333333333333333
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
